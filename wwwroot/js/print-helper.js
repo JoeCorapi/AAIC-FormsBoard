17b3d2ec079b906825d4ac2862a50ebf9bce8478
JoeCorapi <24542019+JoeCorapi@users.noreply.github.com>
2025-05-12 23:13:34 -0400

    MIlage Testing candidate

---
+++ wwwroot/js/print-helper.js
@@ -0,0 +1,21 @@
+﻿window.printHelper = {
+    printElement: function (element) {
+        // Save the current body content
+        var originalContent = document.body.innerHTML;
+
+        // Get the content of the element to print
+        var printContent = element.innerHTML;
+
+        // Replace the body content with just what we want to print
+        document.body.innerHTML = printContent;
+
+        // Print the page
+        window.print();
+
+        // Restore the original content
+        document.body.innerHTML = originalContent;
+
+        // Reload any JavaScript that might have been affected
+        window.location.reload();
+    }
+};
